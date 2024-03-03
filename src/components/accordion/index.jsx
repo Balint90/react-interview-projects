7d2e6b7d80da328b4733f208bca736b454896aa1
Bálint Csertán <csertanb@gmail.com>
2024-03-03 13:00:38 +0100

    Add Accordion

---
+++ src/components/accordion/index.jsx
@@ -0,0 +1,57 @@
+//Single selection
+//Multiple Selection
+
+import data from "./data";
+import { useState } from "react";
+import './styles.css';
+
+export default function Accordion() {
+    const [selected, setSelected] = useState(null);
+    const [enableMultiSelection, setEnabledMultiSelection] = useState(false);
+    const [multiple, setMultiple] = useState([]);
+
+    function handleSingleSelection(currentId) {
+        setSelected(currentId === selected ? null : currentId);
+    }
+
+    function handleMultipleSelection(currentId) {
+        let copyMultiple = [...multiple];
+        const findIndexOfCurrentId = copyMultiple.indexOf(currentId);
+
+        if (findIndexOfCurrentId === -1) {
+            copyMultiple.push(currentId);
+        } else {
+            copyMultiple.splice(findIndexOfCurrentId, 1);
+        }
+        setMultiple(copyMultiple);
+    }
+
+    return <div className="wrapper">
+        <button onClick={
+            () => { setEnabledMultiSelection(!enableMultiSelection); setMultiple([]); setSelected(null); }
+        }>
+            Enable Multi Selection
+        </button>
+        <div className="accordion">
+            {
+                data && data.length > 0 ? data.map(dataItem =>
+                    <div className="item" key={dataItem.id}>
+                        <div onClick={
+                            enableMultiSelection
+                                ? () => handleMultipleSelection(dataItem.id)
+                                : () => handleSingleSelection(dataItem.id)
+                        }
+                            className="title">
+                            <h3>{dataItem.question}</h3>
+                            <span>+</span>
+                        </div>
+                        {
+                            selected === dataItem.id || multiple.indexOf(dataItem.id) !== -1 ? <div className="content">{dataItem.answer}</div> : null
+                        }
+                    </div>
+                )
+                    : <div>No data found!</div>
+            }
+        </div>
+    </div >
+}
