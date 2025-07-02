45caf55f9d14bc5b60571451c2c3082dbd7c2089
Bálint Csertán <csertanb@gmail.com>
2025-07-02 23:31:20 +0200

    RandomColor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Accordion from './components/accordion';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
-      <Accordion></Accordion>
+      {/* Accordion component */}
+      {/* <Accordion /> */}
+      {/* Random color component */}
+      <RandomColor />
     </div>
   );
 }
