c4a831a3131b84ebae0f755ec0f77e8b478c5584
Bálint Csertán <csertanb@gmail.com>
2025-08-03 10:55:34 +0200

    Close on "outside" click

--- src/components/CustomModalPopup/modal.jsx
+++ src/components/CustomModalPopup/modal.jsx
@@ -1,8 +1,8 @@
 import './modal.css';
 
 export default function Modal({ id, header, body, footer, onClose }) {
-    return <div id={id || 'Modal'} className="custom-modal">
-        <div className="custom-modal-content">
+    return <div id={id || 'Modal'} className="custom-modal" onClick={onClose}>
+        <div className="custom-modal-content" onClick={(e) => e.stopPropagation()}>
             <div className="header">
                 <span className="close-modal-icon" onClick={onClose}>&times;</span>
                 <h2>{header ? header : 'Header'}</h2>
